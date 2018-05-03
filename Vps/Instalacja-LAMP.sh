006990f56ce6132f00c13ea023eac2d4c3d380ab
Breakermind <hello@breakermind.com>
2018-05-03 20:27:53 +0200

    Instalacja serwera www vps debian 9
    Instalacja serwera www vps debian 9

--- Vps/Instalacja-LAMP.sh
+++ Vps/Instalacja-LAMP.sh
@@ -3,7 +3,10 @@ nano /etc/apt/sources.list
 # Na końcu:
 main contrib non-free
 
-# Instalacja serwera www
+# aktualizuj
+apt-get update
+
+# Instalacja serwera www, mysql
 apt-get install apache2 php php-mysql php-gd php-mbstring php-mcrypt mysql-server
 mysql_secure_installation
 a2enmod ssl rewrite
@@ -15,14 +18,23 @@ nano /etc/php/7.0/apache2/php.ini
 # Zmień i kilka innych
 extension=php_pdo_mysql.dll
 
-# Włączyć rewrite w pliku >Directory /var/www/>
+# Włączyć rewrite w pliku <Directory /var/www/>
 nano /etc/apache2/apache2.conf
 # Zmień None na All
 AllowOverride All
 
-# If commection mysql error from php
+# Virtual hosts dodaj do 000-default.conf lub do pojedyńczych plików
+nano /etc/apache2/sites-available/000-default.conf
+
+# Gdy pojawia sie błąd połączenia php (dodaj usera do mysql)
+mysql -u root -p
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'toor'; 
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY 'toor';
+
+# Kopia zapasowa wszystkich baz danych
+mysqldump -u root -p --all-databases > all.sql
+# Wybranych baz danych
+mysqldump -u root -p --add-locks --add-drop-database --databases dbname1 dbname2 > db.sql
 
 # desktop MATE
 apt-get install mate-desktop-environment-extras mate-tweak
